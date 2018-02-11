440d08097929076292e1d570b1adb8f21e1b5612
Romantic <595159090004@dpu.ac.th>
2018-02-12 04:43:17 +0700

    Memozied Selectors: 3.Reselect

--- client/src/selectors/articles.js
+++ client/src/selectors/articles.js
@@ -1,10 +1,22 @@
+import { createSelector } from 'reselect'
+
 export function getArticle(state, props) {
   return state.articles.items[+props.match.params.id]
 }
 
+/*
+ OLD function
 export function getArticles(state) {
   return Object.values(state.articles.items)
 }
+
+NEW function use libary 
+    Memozied selectors
+*/
+export const getArticles = createSelector(
+  (state) => state.articles, 
+  (articles) => Object.values(articles.items)
+)
 
 export function getIsArticleLoading(state) {
   return state.articles.isLoading
